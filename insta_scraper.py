e51be73b393e051104c1c5f93c695892473cb4c9
Ace-Of-Snakes <franjolic2307@gmail.com>
2023-01-16 03:17:07 +0100

    able to scrape statistics

--- insta_scraper.py
+++ insta_scraper.py
@@ -9,10 +9,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
 import pandas as pd
-
+import regex as re
+import json
 # used for standard options
 
-
+json_profile = {
+    "username ": "",
+    "biographie ": "",
+    "number_of_followers ": "",
+    "number_of_following ": "",
+    "number_of_posts ": "",
+}
 def getOptions()->webdriver.chrome.options:
     '''Returns the options for the webdriver'''
     options = webdriver.ChromeOptions()
@@ -20,8 +27,8 @@ def getOptions()->webdriver.chrome.options:
     # options.add_argument('--incognito')
     # options.add_argument('--headless')
     # user_ agent is used to simulate a non-headless browser, to avoid access denial
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    options.add_argument('user-agent={0}'.format(user_agent))
+    # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    # options.add_argument('user-agent={0}'.format(user_agent))
     # options.add_argument('--no-sandbox')
     options.add_argument('--log-level=1')
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -37,7 +44,7 @@ def startSession()->webdriver:
     driver = webdriver.Chrome(service=Service(
         ChromeDriverManager().install()), options=getOptions())
     # initializes the user agent
-    driver.execute_script("return navigator.userAgent")
+    # driver.execute_script("return navigator.userAgent")
     return driver
 
 # endSession() is used to close the browser
@@ -49,7 +56,38 @@ def acceptCookies():
     except TimeoutException:
         print('No cookies to accept')
 
+def get_number_of_posts()->int:
+    '''Returns the number of posts'''
+    try:
+        html = driver.page_source
+        time.sleep(10)
+        number_regex = re.search(r'''\d+ Follower, \d+ gefolgt, \d+ Beitr''', html)
+        numbers = re.findall(r'\d+', number_regex.group(0))
+        number_of_followers = driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/header/section/ul/li[2]/button/div/span/span').text
+        number_of_following = driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/header/section/ul/li[3]/button/div/span/span').text
+        biographie = driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/header/section/div[3]/h1').text
+        # number_of_followers = int(numbers[0])
+        # number_of_following = int(numbers[1])
+        number_of_posts = int(numbers[2])
+        print(f'Number of followers: {number_of_followers}')
+        print(f'Number of following: {number_of_following}')
+        print(f'Number of posts: {number_of_posts}')
+        print(f'Biographie: {biographie}')
+        
+        json_profile["username "] = client_username
+        json_profile["biographie "] = biographie
+        json_profile["number_of_followers "] = number_of_followers
+        json_profile["number_of_following "] = number_of_following
+        json_profile["number_of_posts "] = number_of_posts
+        with open('profile.json', 'w') as f:
+            json.dump(json_profile, f)
+
+        return number_of_posts
+    except TimeoutException:
+        print('Number of posts not found')
+
 def main():
+    global client_username
     '''Main function'''
     # start the session
     driver = startSession()
@@ -60,6 +98,8 @@ def main():
 
     # accept the cookies
     acceptCookies()
+    # get the number of posts
+    number_of_posts = get_number_of_posts()
     # wait for the page to load
     time.sleep(100)
     # find the username and password fields
