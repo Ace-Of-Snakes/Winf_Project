4467cb878cb8ac51797b397b2d6db53cc40872c8
Ace-Of-Snakes <frankojolic2307@gmail.com>
2023-02-19 21:46:05 +0100

    some pushing

--- calculator.py
+++ calculator.py
@@ -5,66 +5,83 @@ import numpy as np
 import pandas as pd
 
 
-# reach_list,engagement_ratios = [],[]
-# hashtag_dict, word_dict, emoji_dict = {},{},{}
-# data = json.load(open('jonasroeber.json'))
-# followers = data["number_of_followers"]
-# post = data["posts"]
-# keys = post.keys()
+df = pd.read_csv("Emoji_Sentiment_Data_v1.0.csv")
+print(df["Emoji"].values)
+reach_list,engagement_ratios = [],[]
+hashtag_dict, word_dict, emoji_dict = {},{},{}
+data = json.load(open('jonasroeber.json'))
+followers = data["number_of_followers"]
+post = data["posts"]
+keys = post.keys()
 
-# def get_reach_engagement(post, followers):
-#     reach = post["likes"]
-#     engagement_ratio = reach/followers
-#     return reach, engagement_ratio
+def calculate_sentiment(emoji_dict):
+    sentiment= [0,0,0] #negative, neutral, positive
+    for emoji in emoji_dict.keys():
+        for i in range(len(df)):
+            if emoji == df["Emoji"][i]:
+                sentiment[0] += df["Negative"][i] * emoji_dict[emoji]
+                sentiment[1] += df["Neutral"][i] * emoji_dict[emoji]
+                sentiment[2] += df["Positive"][i] * emoji_dict[emoji]
+    overall_sentiment = np.sum(sentiment)
+    sentiment = [x/overall_sentiment for x in sentiment]
+    return sentiment
 
-# def get_hashtags(post):
-#     hashtags = re.findall(r"#\w+", post["text"]["description"])
-#     return hashtags
+def get_reach_engagement(post, followers):
+    reach = post["likes"]
+    engagement_ratio = reach/followers
+    return reach, engagement_ratio
 
-# def get_words(post):
-#     word_keys = post["text"]["comments"].keys()
-#     words = ""
-#     for key in word_keys:
-#         words += post["text"]["comments"][key]
-#     emojis = re.findall(r"\p{Emoji}", words)
-#     words = re.findall(r"\w+", words)
-#     return words, emojis
+def get_hashtags(post):
+    hashtags = re.findall(r"#\w+", post["text"]["description"])
+    return hashtags
 
-# for key in keys:
-#     reach, engagement_ratio = get_reach_engagement(post[key], followers)
-#     hashtags = get_hashtags(post[key])
+def get_words(post):
+    word_keys = post["text"]["comments"].keys()
+    words = ""
+    for key in word_keys:
+        words += post["text"]["comments"][key]
+    emojis = re.findall(r"\p{Emoji}", words)
+    words = re.findall(r"\w+", words)
+    return words, emojis
 
-#     for hashtag in hashtags:
-#         if hashtag in hashtag_dict:
-#             hashtag_dict[hashtag] += 1
-#         else:
-#             hashtag_dict[hashtag] = 1
-#     words, emojis = get_words(post[key])
-#     for word in words:
-#         if word in word_dict:
-#             word_dict[word] += 1
-#         else:
-#             word_dict[word] = 1
+for key in keys:
+    reach, engagement_ratio = get_reach_engagement(post[key], followers)
+    hashtags = get_hashtags(post[key])
 
-#     for emoji in emojis:
-#         if emoji in emoji_dict:
-#             emoji_dict[emoji] += 1
-#         else:
-#             emoji_dict[emoji] = 1
-#     engagement_ratios.append(engagement_ratio)
-#     reach_list.append(reach)
-#     print(f"""{key}: {reach}""")
+    for hashtag in hashtags:
+        if hashtag in hashtag_dict:
+            hashtag_dict[hashtag] += 1
+        else:
+            hashtag_dict[hashtag] = 1
+    words, emojis= get_words(post[key])
+    for word in words:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
 
-# print(f"""{reach_list},{len(reach_list)}""")
-# sorted_hashtags = sorted(hashtag_dict.items(), key=lambda x: x[1], reverse=True)
-# sorted_words = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
-# sorted_emojis = sorted(emoji_dict.items(), key=lambda x: x[1], reverse=True)
+    for emoji in emojis:
+        if emoji in emoji_dict:
+            emoji_dict[emoji] += 1
+        else:
+            emoji_dict[emoji] = 1
+    engagement_ratios.append(engagement_ratio)
+    reach_list.append(reach)
+    print(f"""{key}: {reach}""")
+
+print(f"""{reach_list},{len(reach_list)}""")
+sorted_hashtags = sorted(hashtag_dict.items(), key=lambda x: x[1], reverse=True)
+sorted_words = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+sorted_emojis = sorted(emoji_dict.items(), key=lambda x: x[1], reverse=True)
+print(calculate_sentiment(emoji_dict))
 # print(sorted_hashtags)
 # print(sorted_words)
 # print(sorted_emojis)
-# plt.plot(list(reversed(reach_list)))
-# plt.stem(list(reversed(engagement_ratios)))
-# plt.show()
-
-df = pd.read_csv("Emoji_Sentiment_Data_v1.0.csv")
-print(df)
+plt.plot(list(reversed(reach_list)))
+plt.figure()
+plt.plot(np.linspace(0,len(post), len(post)), np.ones(len(post))*(np.sum(engagement_ratios)/len(engagement_ratios)), label="average", color="red")
+plt.stem(list(reversed(engagement_ratios)))
+# pie chart of sentiment values
+plt.figure()
+plt.pie(calculate_sentiment(emoji_dict), labels=["negative", "neutral", "positive"], autopct='%1.1f%%')
+plt.show()
