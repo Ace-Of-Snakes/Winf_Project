cb8a689471f557054d91a10a3f83f14882da4b6a
Ace-Of-Snakes <frankojolic2307@gmail.com>
2023-02-23 04:34:53 +0100

    fixing jans website

---
+++ html shit/func.js
@@ -0,0 +1,11 @@
+var v = 0;
+
+function changeborder(el) {
+    
+    if(el.style.border == "none"){
+    el.style.border = "5px solid #8DF7FF";
+    }
+    else{
+        el.style.border = "none";
+    }
+}
